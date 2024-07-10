155693be39dda5a20b8b9dfe3bb2ff2552c3239b
EddieCodder <ed.durand.obando@gmail.com>
2024-07-10 12:40:44 -0500

    Diseño del header

--- ecoarequipa/src/components/Header.js
+++ ecoarequipa/src/components/Header.js
@@ -1,20 +1,21 @@
 // components/Header.js
 import React from 'react';
-import './Header.css';
+import './Header.css'; // Importa el archivo de estilos CSS
+import logo from '../assets/logoEcoArequipa.png'; // Importa la imagen del logo
 
 const Header = () => {
   return (
     <header className="header">
       <div className="logo">
-        <img src="/logo.svg" alt="Logo" />
+        <img src={logo} alt="Logo" />
         <span>Nombre de tu Aplicación</span>
       </div>
       <nav className="nav">
         <ul>
           <li><a href="/">Inicio</a></li>
-          <li><a href="/Juegos">Nosotros</a></li>
-          <li><a href="/Noticias">Nosotros</a></li>
-          <li><a href="/Nosotros">Nosotros</a></li>
+          <li><a href="/juegos">Juegos</a></li>
+          <li><a href="/noticias">Noticias</a></li>
+          <li><a href="/nosotros">Nosotros</a></li>
           {/* Agrega más enlaces según sea necesario */}
         </ul>
       </nav>
