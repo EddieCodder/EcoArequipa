166842bad5b8f0222f620c95cb66b6de11365b1d
kevinPedroYare <kyare@unsa.edu.pe>
2024-07-11 17:00:29 -0500

    se añadio un boton para mandar a la pagina de juegos

--- ecoarequipa/src/components/Juego.js
+++ ecoarequipa/src/components/Juego.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Juego = ({ title, description, image }) => {
+const Juego = ({ title, description, image, link }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(link);
+  };
+
   return (
     <div className="juego">
       <img src={image} alt={title} className="juego-image" />
       <h2>{title}</h2>
       <p>{description}</p>
+      <button className="juego-button" onClick={handleClick}>Jugar</button>
     </div>
   );
 };
