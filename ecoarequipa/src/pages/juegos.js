910918871b86e0c9217741ea6bf17d59b66ba433
kevinPedroYare <kyare@unsa.edu.pe>
2024-07-10 23:57:39 -0500

    se modifico el archivo juegos para la muestra de los juegos

--- ecoarequipa/src/pages/juegos.js
+++ ecoarequipa/src/pages/juegos.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import ListaDeJuegos from '../components/ListaDeJuegos';
 import '../styles.css';
 
 const Juegos = () => {
   return (
     <div className="juegos">
       <h1>Juegos</h1>
-      <p>Este es el contenido de la página de juegos.</p>
+      <p>Esta es la sección de juegos educativos para el cuidado del medio ambiente. ¡Diviértete mientras aprendes a cuidar nuestro planeta!</p>
+      <ListaDeJuegos />
     </div>
   );
 }
