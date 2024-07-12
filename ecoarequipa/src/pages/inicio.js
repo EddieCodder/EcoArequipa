d3d4aa627c3990cb600429f1bee58bb69c375e81
EddieCodder <ed.durand.obando@gmail.com>
2024-07-12 12:28:00 -0500

    Agregando más contenido a inicio

--- ecoarequipa/src/pages/inicio.js
+++ ecoarequipa/src/pages/inicio.js
@@ -1,38 +1,58 @@
 import React from 'react';
-import '../styles.css'; // Asegúrate de importar correctamente los estilos generales
+import '../styles.css'; 
+import text1 from '../assets/text1.png';
+import text2 from '../assets/text2.png';
+import text3 from '../assets/text3.png';
+import objetivo from '../assets/objetivo.png';
+import juegos from '../assets/juegos.png';
+import noticias from '../assets/noticias.png';
+import sobreNosotros from '../assets/sobre-nosotros.png';
+
 
 const Inicio = () => {
   return (
     <div className="inicio">
       <section className="background-section">
         <div className="hero">
-          <h1>Bienvenidos a EcoArequipa</h1>
-          <p>¡Explora y aprende sobre la importancia de cuidar nuestro medio ambiente!</p>
+          <img src={text1} style={{ width: '500px', height: 'auto' }} alt="Texto 1" />
+          <img src={text2} alt="Texto 2" />
+          <img src={text3} style={{ width: '1000px', height: 'auto' }} alt="Texto 3" />
         </div>
       </section>
-      <section className="objective-section">
-        <div className="objective-content">
-          <h1>Nuestro Objetivo</h1>
-          <p>EcoArequipa se dedica a promover la conciencia ambiental entre los niños utilizando juegos interactivos y contenido educativo usando escenarios de nuestra linda ciudad.</p>
+      <section className="objective-section section-right">
+        <div className="section-content">
+          <div className="section-text">
+            <h2 className="section-title">Nuestro Objetivo</h2>
+            <p className="section-description">EcoArequipa se dedica a promover la conciencia ambiental entre los niños utilizando juegos interactivos y contenido educativo con escenarios de nuestra linda ciudad. La plataforma está desarrollada especialmente para los niños de Arequipa con el objetivo de concientizarlos sobre la importancia del cuidado del medio ambiente a través de actividades lúdicas y educativas que reflejan la belleza y los desafíos ambientales de nuestra región.</p>
+          </div>
+          <img className="section-image" src={objetivo} alt="Imagen de objetivo" />
         </div>
       </section>
-      <section className="content-section">
-        <div className="content">
-          <h2>Más Información</h2>
-          <p>En esta sección, puedes agregar más detalles sobre tu proyecto, eventos próximos, noticias relevantes, o cualquier otra información que desees destacar.</p>
+      <section className="games-section section-left">
+        <div className="section-content">
+          <img className="section-image" src={juegos} alt="Imagen de juegos" />
+          <div className="section-text">
+            <h2 className="section-title">Juegos</h2>
+            <p className="section-description">Descubre nuestra colección de juegos diseñados para enseñar a los niños sobre el cuidado ambiental en Arequipa. Cada juego ofrece una experiencia interactiva única que combina diversión y aprendizaje sobre los desafíos y la belleza de nuestra región.</p>
+          </div>
         </div>
       </section>
-      <section className="activities-section">
-        <div className="activities">
-          <h2>Actividades</h2>
-          <p>Aquí puedes describir algunas de las actividades que ofrecemos, como talleres, charlas, y actividades interactivas para los niños.</p>
+      <section className="news-section section-right">
+        <div className="section-content">
+          <div className="section-text">
+            <h2 className="section-title">Noticias</h2>
+            <p className="section-description">Mantente al día con las últimas noticias y eventos relacionados con el medio ambiente en Arequipa. Aquí encontrarás información relevante sobre iniciativas locales, proyectos ambientales y actividades comunitarias para el cuidado de nuestro entorno.</p>
+          </div>
+          <img className="section-image" src={noticias} alt="Imagen de noticias" />
         </div>
       </section>
-      <section className="contact-section">
-        <div className="contact">
-          <h2>Contacto</h2>
-          <p>Para más información, puedes ponerte en contacto con nosotros a través de nuestro correo electrónico o redes sociales.</p>
-          
+      <section className="sobre-nosotros-section section-left">
+        <div className="section-content">
+          <img className="section-image" src={sobreNosotros} alt="Imagen de Sobre Nosotros" />
+          <div className="section-text">
+            <h2 className="section-title">Sobre Nosotros</h2>
+            <p className="section-description">Conoce más sobre EcoArequipa y nuestro compromiso con la educación ambiental en Arequipa. Descubre quiénes somos, nuestros valores y cómo puedes unirte a nuestra comunidad para hacer una diferencia positiva en el medio ambiente local.</p>
+          </div>
         </div>
       </section>
     </div>
