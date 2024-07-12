092e725d1e65ad0173ed7551f50f4ed4f835244a
EddieCodder <ed.durand.obando@gmail.com>
2024-07-12 10:58:39 -0500

    Corrigiendo el footer

--- ecoarequipa/src/pages/inicio.js
+++ ecoarequipa/src/pages/inicio.js
@@ -13,6 +13,26 @@ const Inicio = () => {
       <section className="objective-section">
         <div className="objective-content">
           <h1>Nuestro Objetivo</h1>
+          <p>EcoArequipa se dedica a promover la conciencia ambiental entre los niños utilizando juegos interactivos y contenido educativo usando escenarios de nuestra linda ciudad.</p>
+        </div>
+      </section>
+      <section className="content-section">
+        <div className="content">
+          <h2>Más Información</h2>
+          <p>En esta sección, puedes agregar más detalles sobre tu proyecto, eventos próximos, noticias relevantes, o cualquier otra información que desees destacar.</p>
+        </div>
+      </section>
+      <section className="activities-section">
+        <div className="activities">
+          <h2>Actividades</h2>
+          <p>Aquí puedes describir algunas de las actividades que ofrecemos, como talleres, charlas, y actividades interactivas para los niños.</p>
+        </div>
+      </section>
+      <section className="contact-section">
+        <div className="contact">
+          <h2>Contacto</h2>
+          <p>Para más información, puedes ponerte en contacto con nosotros a través de nuestro correo electrónico o redes sociales.</p>
+          
         </div>
       </section>
     </div>
