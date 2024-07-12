ea2cfabe21d1cea0892818fae6d9a3ddb2bd8b3a
kevinPedroYare <kyare@unsa.edu.pe>
2024-07-12 18:21:09 -0500

    se añadio la imagen en el titulo

--- ecoarequipa/src/pages/juegos.js
+++ ecoarequipa/src/pages/juegos.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ListaDeJuegos from '../components/ListaDeJuegos';
 import '../styles.css';
+import juegosImagen from '../assets/juegos-text.png';
 
 const Juegos = () => {
   return (
     <div className="juegos">
-      <h1>Juegos</h1>
+      <img src={juegosImagen} alt='juegos-text' className='juegos-text' />
       <p>Esta es la sección de juegos educativos para el cuidado del medio ambiente. ¡Diviértete mientras aprendes a cuidar nuestro planeta!</p>
       <ListaDeJuegos />
     </div>
