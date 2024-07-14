7148ce439ec8c5b07925a2582bc963d0f3e956de
inando4 <torresluisperez@hotmail.com>
2024-07-13 22:15:43 -0500

    ga

--- ecoarequipa/src/pages/noticias.js
+++ ecoarequipa/src/pages/noticias.js
@@ -15,6 +15,16 @@ const publicaciones = [
       titulo: 'GRAN FERIA AMBIENTAL POR LA HORA DEL PLANETA', 
       contenido: '¡Únete a la Gran Feria Ambiental por «La Hora del Planeta»! Este sábado 23 de marzo, te esperamos en la plaza Mayta Cápac de Miraflores para un evento dedicado al medio ambiente. Habrá exposiciones sobre temas ambientales, juegos y talleres para todas las edades. ¡Ven y sé parte de esta bonita actividad! Premiaremos tu participación y juntos apagaremos las luces por 1 hora.', 
       imagen: evento1 },
+  { id: 1, 
+      tipo: 'noticia', 
+      titulo: 'CONSERVACION DE ÁREA NATURAL PROTEGIDA QUE GARANTIZARÁ AGUA EN AREQUIPA', 
+      contenido: 'En Arequipa, Perú, se han destinado más de 4 millones de soles para proteger un lugar muy especial llamado Reserva Nacional Salinas y Aguada Blanca. Este lugar es vital porque nos asegura tener agua limpia para beber, y también es hogar de muchos animales y plantas. Las comunidades locales están ayudando mucho para cuidar este lugar mágico. ¡Es como un gran equipo trabajando juntos para salvar la naturaleza y asegurar que todos tengamos agua limpia! ¿No es increíble?', 
+      imagen: noticia1 },
+      { id: 1, 
+        tipo: 'noticia', 
+        titulo: 'CONSERVACION DE ÁREA NATURAL PROTEGIDA QUE GARANTIZARÁ AGUA EN AREQUIPA', 
+        contenido: 'En Arequipa, Perú, se han destinado más de 4 millones de soles para proteger un lugar muy especial llamado Reserva Nacional Salinas y Aguada Blanca. Este lugar es vital porque nos asegura tener agua limpia para beber, y también es hogar de muchos animales y plantas. Las comunidades locales están ayudando mucho para cuidar este lugar mágico. ¡Es como un gran equipo trabajando juntos para salvar la naturaleza y asegurar que todos tengamos agua limpia! ¿No es increíble?', 
+        imagen: noticia1 }
 
   // Agrega más noticias y eventos según sea necesario
 ];
